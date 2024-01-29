83437fb9fc5eaed50ea97081adf31ca738f374be
Tess Rezard <tessrezard@gmail.com>
2024-01-29 20:54:14 +0000

    add preset subreddits to nav menu

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -12,34 +12,21 @@ function NavMenu({ closeDropdown }) {
         { id: 1, name: 'Technology', slug: 'technology' },
         { id: 2, name: 'Science', slug: 'science' },
         { id: 3, name: 'Programming', slug: 'programming' },
-        { id: 4, name: 'Acotar', slug: 'acotar' },
+        { id: 4, name: 'Houseplants', slug: 'houseplants' },
         { id: 5, name: 'Maps', slug: 'MapPorn' },
+        { id: 6, name: 'React', slug: 'react' },
+        { id: 7, name: 'Today I Learned', slug: 'todayilearned' },
+        { id: 8, name: 'Pop culture chat', slug: 'popculturechat' },
+        { id: 9, name: 'Acotar', slug: 'acotar' },
+        { id: 10, name: 'Swimming', slug: 'swimming' },
 
 
 
     ]
 
-
-    useEffect(() => {
-        const handleClickOutside = (event) => {
-            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-                console.log('hi barbie');
-                // Clicked outside the dropdown, close it
-                // closeDropdown();
-            }
-        };
-
-        document.addEventListener("mousedown", handleClickOutside);
-
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, [closeDropdown]);
-
-
     return (
         <>
-            <div className='menu-container' ref={dropdownRef}>
+            <div className='menu-container' >
                 <div className="menu-section">
                     <div className="section-title-container">
                         <p className="section-title">
