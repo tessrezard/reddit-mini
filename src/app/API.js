78581068743bd180dad6af2073c6eaac5bfe07ea
Tess Rezard <tessrezard@gmail.com>
2024-02-12 09:53:59 +0000

    add caching to about API call to help 429 (Too Many Errors) issue

--- src/app/API.js
+++ src/app/API.js
@@ -1,5 +1,3 @@
-import React from 'react';
-
 export const API_ROOT = 'https://www.reddit.com';
 export const SUBREDDITS_ENDPOINT = `${API_ROOT}/subreddits.json`;
 
@@ -81,20 +79,50 @@ export const getPostComments = async (permalink) => {
 
 //GET ABOUT SUBREDDIT
 
+// export const getAboutSubreddit = async (subreddit) => {
+//   try {
+//     const response = await fetch(`${API_ROOT}/r/${subreddit}/about.json`);
+//     if (!response.ok) {
+//       throw new Error(`Error fetching data for ${subreddit}: ${response.statusText}`);
+//     }
+//     const json = await response.json();
+//     return json.data;
+
+//   } catch (error) {
+//     console.error(`Error fetching the 'about' data for ${subreddit}:`, error);
+//     throw error;
+//   }
+// };
+
+
+
 export const getAboutSubreddit = async (subreddit) => {
   try {
+    // Check if data is already in local storage
+    const cachedData = localStorage.getItem(`aboutSubreddit_${subreddit}`);
+
+    if (cachedData) {
+      // If data is in the cache, return it
+      const parsedData = JSON.parse(cachedData);
+      // console.log(`Using cached 'about' data for ${subreddit}:`, parsedData);
+      return parsedData;
+    }
+
+    // If data is not in the cache, fetch from the API
     const response = await fetch(`${API_ROOT}/r/${subreddit}/about.json`);
+    
     if (!response.ok) {
       throw new Error(`Error fetching data for ${subreddit}: ${response.statusText}`);
     }
-    const json = await response.json();
-    return json.data;
 
+    const json = await response.json();
+
+    // Store data in local storage
+    localStorage.setItem(`aboutSubreddit_${subreddit}`, JSON.stringify(json.data));
+    // console.log(`Fetched and cached 'about' data for ${subreddit}:`, json.data);
+    return json.data;
   } catch (error) {
     console.error(`Error fetching the 'about' data for ${subreddit}:`, error);
     throw error;
   }
 };
-
-
-
